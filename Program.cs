6d09c5e5e5fae8da5ed7e7094a3ff5204c1f3689
samuelkissner <samuelkissner@gmail.com>
2020-02-25 11:09:41 -0600

    added user input for investment variables and validated this input

--- Program.cs
+++ Program.cs
@@ -10,9 +10,44 @@ namespace InvestmentGrowthEstimator
     {
         static void Main(string[] args)
         {
-            double balance = 30000;
-            double annualContribution = 35500;
-            double annualGrowthRate = 1.07;
+            string errorMsg = "Please enter a positive, numeric value.";
+            string input;
+
+            //obtain and validate user input for starting value
+            double balance;
+            while(true){
+                Console.Write("Enter starting balance: ");
+                input = Console.ReadLine().Trim();
+                if(!double.TryParse(input, out balance) || balance <0){
+                    Console.WriteLine(errorMsg);
+                }else{
+                    break;
+                }
+            }
+            //obtain and validate user input for annual contribution
+            double annualContribution;
+            while(true){
+                Console.Write("Enter annual contribution: ");
+                input = Console.ReadLine().Trim();
+                if(!double.TryParse(input, out annualContribution) || balance <0){
+                    Console.WriteLine(errorMsg);
+                }else{
+                    break;
+                }
+            }
+
+            //obtain and validate user input for growth rate
+            double annualGrowthRate;
+            while(true){
+                Console.Write("Enter annual growth rate (1.07 would be a growth rate of 7%): ");
+                input = Console.ReadLine().Trim();
+                if(!double.TryParse(input, out annualGrowthRate) || balance <0){
+                    Console.WriteLine(errorMsg);
+                }else{
+                    break;
+                }
+            }
+            
             int i = 0;
             string columnHeaders = "Year     Balance";
             // the following is the index at which the 'B' is.
